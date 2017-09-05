fc0a08f591b3e48c02de44e34fc762f5795aad66
Andrew Head <head.andrewm@gmail.com>
2017-09-04 20:35:30 -0700

    Slices: Updated with most recent versions of examples

--- slices/FetchRowSlice.java
+++ slices/FetchRowSlice.java
@@ -10,29 +10,34 @@ import java.util.List;
 
 public class FetchRowSlice {
 
-    public static void main(String[] args) throws ConnectionException {
-        String QUERY = "SELECT id, title, year, num_pages FROM table WHERE title LIKE '%" + "romance" + "%'";
-        int COLUMN_INDEX_TITLE = 1;
-        Database database = new Database("lou", "PA$$W0RD", "https://acme-books.com/db");
-        Cursor cursor = database.cursor();
-        cursor.execute(QUERY);
-        boolean finished = false;
-        if (cursor.rowCount() > 0) {
-            int rowNumber = 0;
-            while (!finished) {
-                int rowCount = cursor.rowCount();
-                for (int i = 0; i < Math.min(rowCount, maxBooks); ++i) {
-                    cursor.fetchone();
-                    String title = cursor.getString(COLUMN_INDEX_TITLE);
-                    rowNumber++;
-                    if (cursor.end() || rowNumber >= maxBooks) {
-                        finished = true;
-                    } else if (i == rowCount - 1){
-                        cursor.next(id);
-                    }
-                }
-             }
+  public static void main(String[] args) throws ConnectionException {
+
+    int maxBooks = 3;
+    String QUERY = "SELECT id, title, year, num_pages FROM table WHERE title LIKE '%" + "romance" + "%'";
+    int COLUMN_INDEX_ID = 0;
+    int COLUMN_INDEX_TITLE = 1;
+    Database database = new Database("lou", "PA$$W0RD", "https://acme-books.com/db");
+    Cursor cursor = database.cursor();
+    cursor.execute(QUERY);
+    boolean finished = false;
+    if (cursor.rowCount() > 0) {
+      int rowNumber = 0;
+      while (finished == false) {
+        int rowCount = cursor.rowCount();
+        for (int i = 0; i < Math.min(rowCount, maxBooks); ++i) {
+          cursor.fetchone();
+          int id = cursor.getInt(COLUMN_INDEX_ID);
+          String title = cursor.getString(COLUMN_INDEX_TITLE);
+          rowNumber++;
+          if (cursor.end() || rowNumber >= maxBooks) {
+              finished = true;
+          }
+          if (finished == false && i == rowCount - 1) {
+              cursor.next(id);
+          }
         }
+      }
     }
 
+  }
 }
--- slices/ScrapeTextSlice.java
+++ slices/ScrapeTextSlice.java
@@ -5,22 +5,25 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class ScrapeImageLinksSlice {
+public class ScrapeTextSlice {
 
-    public static void main(String[] args) throws IOException {
-        args = new String[]{"electric bike", "openairandrew@gmail.com"};
-        String query = URLEncoder.encode(args[0], "utf-8");
-        String destination = args[1];
-        URL url = new URL("https://sfbay.craigslist.org/search/bia?query=" + query);
-        Document doc = Jsoup.parse(url, 3*1000);
-        Elements rows = doc.select("li.result-row");
-        for (Iterator rowsIterator = rows.iterator(); rowsIterator.hasNext();) {
-            Element row = (Element) rowsIterator.next();
-            String title = row.select("a.result-title.hdrlnk").text();
-        }
+  public static void main(String[] args) throws IOException {
+
+    String arg0 = "electric bike";
+    String arg1 = "openairandrew@gmail.com";
+    String query = URLEncoder.encode(arg0, "utf-8");
+    String destination = arg1;
+    URL url = new URL("https://sfbay.craigslist.org/search/bia?query=" + query);
+    Document doc = Jsoup.parse(url, 3*1000);
+    Elements rows = doc.select("li.result-row");
+    for (Iterator rowsIterator = rows.iterator(); rowsIterator.hasNext();) {
+      Element row = (Element) rowsIterator.next();
+      String title = row.select("a.result-title.hdrlnk").text();
     }
+
+  }
+
 }
